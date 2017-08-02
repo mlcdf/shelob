649ddaab4d2409376ef7e6400c60e3ae80f7fb0e
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-08-02 23:46:14 +0200

    feat: add pretty-printing

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const { send } = require('micro');
 const { router, get } = require('microrouter');
+const querystring = require('querystring');
+const stringifyObject = require('stringify-object');
 const url = require('url');
 
 const extract = require('./lib/extract');
 
 const index = async (req, res) => {
+  const query = querystring.parse(url.parse(req.url).query);
   await extract(req.params.username, req.params.category)
     .then(data => {
-      send(res, 200, data);
+      if (query.pretty === 'true') {
+        const pretty = stringifyObject(data, {
+          indent: '  ',
+          singleQuotes: false
+        });
+        send(res, 200, pretty);
+      } else {
+        send(res, 200, data);
+      }
     })
     .catch(() => {
       send(res, 500, { error: 'Something happened' });
