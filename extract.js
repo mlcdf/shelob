3dd8a409e50f618e4dbe36012cc9efe92873c49d
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2017-08-12 00:12:34 +0200

    improve code logic & quality

--- extract.js
+++ extract.js
@@ -2,8 +2,10 @@ const bluebird = require('bluebird');
 const cheerio = require('cheerio');
 const got = require('got');
 
-async function extractStats(username, category) {
-  console.log(`${username}/${category}`);
+module.exports = async function(username, category) {
+  const collection = [];
+
+  // Crawl the first page
   const url = `https://www.senscritique.com/${username}/collection/rating/${category}/all/all/all/all/all/all/all/page-1`;
   let response;
 
@@ -14,30 +16,43 @@ async function extractStats(username, category) {
     throw err;
   }
 
-  const $ = cheerio.load(response.body);
+  // Then extract data from the first page
+  const stats = extractStats(response.body);
+  collection.push(...extractItems(response.body, category));
+
+  const nbOfPages = Math.ceil(stats['rated'] / 18); // 18 being the number of item per page
+
+  if (nbOfPages >= 1) {
+    const indexes = Array.from({ length: nbOfPages }, (v, k) => k + 2); // Build a [] from 2 => nbOfPages
+
+    await bluebird.map(indexes, async function(index, callback) {
+      const url =
+        `https://www.senscritique.com/${username}/collection/rating/${category}/all/all/all/all/all/all/all/page-` +
+        index;
+      const response = await got(url);
+      const items = extractItems(response.body, category);
+      collection.push(...items);
+    });
+  }
+
+  return { stats, collection };
+};
+
+/**
+ * Extract some stats on the user's collection
+ * @param {String} data
+ */
+function extractStats(data) {
+  const $ = cheerio.load(data);
 
   const stats = {
-    watchlisted: parseInt(
-      $('[data-sc-collection-filter="wish"] span span')
-        .text()
-        .trim()
-        .slice(1, -1)
-    ),
-    rated: parseInt(
-      $('[data-sc-collection-filter="rating"] span span')
-        .text()
-        .trim()
-        .slice(1, -1)
-    ),
-    finished: parseInt(
-      $('[data-sc-collection-filter="done"] span span')
-        .text()
-        .trim()
-        .slice(1, -1)
-    )
+    watchlisted: $('[data-sc-collection-filter="wish"] span span'),
+    rated: $('[data-sc-collection-filter="rating"] span span'),
+    finished: $('[data-sc-collection-filter="done"] span span')
   };
 
   for (let key of Object.keys(stats)) {
+    stats[key] = parseInt(stats[key].text().trim().slice(1, -1)); // Remove \t, spaces, and ()
     if (Object.is(stats[key], NaN)) {
       stats[key] = 0;
     }
@@ -46,58 +61,49 @@ async function extractStats(username, category) {
   return stats;
 }
 
-module.exports = async function(username, category) {
-  const collection = [];
-  const stats = await extractStats(username, category);
-  const pages = Math.ceil(stats['rated'] / 18);
-  const indexes = Array.from({ length: pages }, (v, k) => k + 1);
+/**
+ * Extract all the items (ie movies, books, ...) from a HTML page
+ * @param {String} data
+ */
+function extractItems(data, category) {
+  const $ = cheerio.load(data);
+  const items = [];
 
-  return await bluebird
-    .map(indexes, async function(index, callback) {
-      const url =
-        `https://www.senscritique.com/${username}/collection/rating/${category}/all/all/all/all/all/all/all/page-` +
-        index;
-      const response = await got(url);
-      const $ = cheerio.load(response.body);
+  $('.elco-collection-item').each(function() {
+    const item = Object.create({});
 
-      $('.elco-collection-item').each(function() {
-        const creativeWork = Object.create({});
+    item.frenchTitle = $(this).find('.elco-title a').text().trim();
 
-        creativeWork.frenchTitle = $(this).find('.elco-title a').text().trim();
+    if (!['morceaux', 'albums'].includes(category)) {
+      const originalTitle = $(this).find('.elco-original-title').text().trim();
+      item.originalTitle =
+        originalTitle !== '' ? originalTitle : item.frenchTitle;
+    }
 
-        if (!['morceaux', 'albums'].includes(category)) {
-          const originalTitle = $(this)
-            .find('.elco-original-title')
-            .text()
-            .trim();
-          creativeWork.originalTitle =
-            originalTitle !== '' ? originalTitle : creativeWork.frenchTitle;
-        }
+    item.year = parseInt($(this).find('.elco-date').text().trim().slice(1, -1));
 
-        creativeWork.year = parseInt(
-          $(this).find('.elco-date').text().trim().slice(1, -1)
-        );
-
-        const creators = [];
-        $(this).find('.elco-product-detail a.elco-baseline-a').each(function() {
-          creators.push($(this).text().trim());
-        });
-        creativeWork[creatorLabel(category)] = creators;
-
-        creativeWork.rating = parseInt(
-          $(this).find('.elco-collection-rating.user > a > div > span').text()
-        );
-
-        collection.push(creativeWork);
-      });
-    })
-    .then(() => {
-      return { username, category, stats, collection };
+    const creators = [];
+    $(this).find('.elco-product-detail a.elco-baseline-a').each(function() {
+      creators.push($(this).text().trim());
     });
-};
+    item[creatorLabel(category)] = creators;
 
-function creatorLabel(category = '') {
-  if (category == 'films') {
+    item.rating = parseInt(
+      $(this).find('.elco-collection-rating.user > a > div > span').text()
+    );
+
+    items.push(item);
+  });
+
+  return items;
+}
+
+/**
+ * Figure out which label to use for the field `creators`
+ * @param {String} category
+ */
+function creatorLabel(category) {
+  if (category === 'films') {
     return 'directors';
   }
   if (category === 'bd') {
--- index.js
+++ index.js
@@ -12,50 +12,44 @@ const setHeaders = handler => (req, res) => {
 
 const pretty = data => JSON.stringify(data, null, '  ');
 
-const index = (req, res) => {
-  send(
-    res,
-    200,
-    pretty({
-      name: 'Shelob',
-      description:
-        "Extract your data (or someone else's) from senscritique.com",
-      usage: 'GET /:username/:category',
-      docs: 'https://github.com/mlcdf/shelob#shelob',
-      src: 'https://github.com/mlcdf/shelob',
-      author: {
-        name: 'Maxime Le Conte des Floris',
-        email: 'hello@mlcdf.com',
-        url: 'https://mlcdf.com'
-      }
-    })
-  );
+const about = {
+  name: 'Shelob',
+  description: "Extract your data (or someone else's) from senscritique.com",
+  usage: 'GET /:username/:category',
+  docs: 'https://github.com/mlcdf/shelob#shelob',
+  src: 'https://github.com/mlcdf/shelob',
+  author: {
+    name: 'Maxime Le Conte des Floris',
+    email: 'hello@mlcdf.com',
+    url: 'https://mlcdf.com'
+  }
 };
+
+const index = (req, res) => send(res, 200, pretty(about));
 
 const api = async (req, res) => {
   await extract(req.params.username, req.params.category)
-    .then(data => {
-      if (req.query.pretty === 'false') {
-        send(res, 200, data);
-      } else {
-        send(res, 200, pretty(data));
-      }
+    .then(({ stats, collection }) => {
+      const json = {
+        username: req.params.username,
+        category: req.params.category,
+        stats,
+        collection
+      };
+      send(res, 200, req.query.pretty === 'false' ? json : pretty(json));
     })
     .catch(err => {
       console.log(err);
-      if (err.statusCode != null) {
-        send(
-          res,
-          err.statusCode,
-          pretty({ code: err.statusCode, message: err.message })
-        );
-      } else {
-        send(res, 500, pretty({ message: 'Something happened' }));
-      }
+      const json = {
+        code: err.statusCode ? err.statusCode : 500,
+        message: err.message ? err.message : 'Something happened'
+      };
+      send(res, json.code, pretty(json));
     });
 };
 
-const notFound = (req, res) => send(res, 404, pretty({ message: 'Not found' }));
+const notFound = (req, res) =>
+  send(res, 404, pretty({ code: 404, message: 'Not found' }));
 
 module.exports = router(
   get('/:username/:category', setHeaders(api)),
